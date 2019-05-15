f94296321082ca37644cc15bd19384ed2204a295
David-Bayly <david.w.bayly@gmail.com>
2019-05-15 13:31:38 -0700

    Created project skeleton. Created folders for pages, partials, tests and styles along with a simple express server.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+let app = express();
+const bodyParser = require("body-parser");
+//Setting up body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//Setting up ejs as the view engine. 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+app.get("/",function(req,res){
+	res.render("pages/index")
+});
+
+app.post("/",function(req,res){
+	console.log(req.body);
+	res.send("Post recieved");
+});
+app.listen(1337);
