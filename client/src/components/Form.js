d95b89ca38b4301560cf656d9b82a9979385e419
David-Bayly <david.w.bayly@gmail.com>
2019-05-16 15:01:00 -0700

    Set up react testing framework and created simple test for Form component.

---
+++ client/src/components/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Form extends Component{
+  constructor(props) {
+  	super(props);
+  }
+  render(){
+  	return(
+  	<form>
+  		<input type="submit"/>
+  	</form>
+  	);
+  }
+}
+
+ 
+export default Form;
