667e9850a9328e00cbfefdad11e80431b9bb8b4c
David-Bayly <david.w.bayly@gmail.com>
2019-05-17 19:58:17 -0700

    Added state to all components.

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 import FoodEntries from "./form-components/FoodEntries"
 class Form extends Component{
-  // constructor(props) {
-  // 	super(props);
-  // }
+  constructor(props) {
+  	super(props);
+  	this.state={
+  		time:"",
+  		foodEntries:[],
+  		location:"",
+  		quantity:1,
+  		repeatDaily:"off",
+  	}
+  	this.changeTime=this.changeTime.bind(this);
+  	this.changeFoodEntries=this.changeFoodEntries.bind(this);
+  	this.changeLocation=this.changeLocation.bind(this);
+  	this.changeQuantity=this.changeQuantity.bind(this);
+  	this.changeRepeat=this.changeRepeat.bind(this);
+  }
+  changeTime(event){
+  	this.setState({time:event.target.value});
+  }
+  changeFoodEntries(value){
+  	this.setState({foodEntries:value});
+  }
+  changeLocation(event){
+  	this.setState({location:event.target.value});
+  }
+  changeQuantity(event){
+  	this.setState({quantity:event.target.value});
+  }
+  changeRepeat(event){
+  	this.setState({repeatDaily:event.target.value});
+  }
   submitForm(event){
-  	console.log("Form Submitted!");
-  	//console.log(event.target.elements);
+  	console.log("Form Submitted!",this.state);
   }
   render(){
   	return(
   	<div>
-  		<form onSubmit={event =>{
+  		<form onSubmit={event=>{
   			event.preventDefault();
   			if(this.props.submitForm!==undefined){
   				this.props.submitForm();
@@ -22,12 +48,13 @@ class Form extends Component{
   		}}>
 	  		<div>
 				<label htmlFor="feedtime">feedtime</label>
-				<input type="time" id="feedtime" required/>
+				<input type="time" id="feedtime" onChange={this.changeTime} required/>
 			</div>
-			<FoodEntries/>
+			<FoodEntries foodEntries={this.state.foodEntries} changeFoodEntries={this.changeFoodEntries}/>
 			<div>
 				<label htmlFor="location">Location</label>
-				<select id="location" name="location" required>
+				<select id="location" name="location" onChange={this.changeLocation} required>
+		            <option value=''></option>
 		            <option value="Portland">Portland</option>
 		            <option value="Seattle">Seattle</option>
 		  		    <option value="Vancouver">Vancouver</option>
@@ -36,8 +63,12 @@ class Form extends Component{
 			</div>
 			<div>
 				<label htmlFor="quantity">quantity</label>
-				<input type="number" id="quantity" min="1" required/>
+				<input type="number" id="quantity" min="1" onChange={this.changeQuantity} required/>
 			</div>
+			<div>
+			<label htmlFor="repeat">Repeat Daily?</label>
+			  <input type="checkbox" id="repeat" name="repeat" onChange={this.changeRepeat}/>
+  			</div>
   			<button type="submit">Submit</button>
   		</form>
   	</div>
--- client/src/components/form-components/FoodEntries.js
+++ client/src/components/form-components/FoodEntries.js
@@ -3,24 +3,25 @@ import FoodEntry from "./FoodEntry"
 class FoodEntries extends Component{
   constructor(props) {
   	super(props);
-    this.state={entries:[],foodtype:null};
+    this.state={
+      foodtype:"",
+    };
     this.addFoodEntry = this.addFoodEntry.bind(this);
     this.selectType = this.selectType.bind(this);
     this.removeFoodEntry = this.removeFoodEntry.bind(this);
   }
   addFoodEntry(event){
-    let entries = this.state.entries;
-    //console.log(event);
+    let entries = this.props.foodEntries;
+    console.log(entries);
     if(this.state.foodtype!==""&&this.state.foodtype!==null){
-      entries.push(this.state.foodtype);
-      this.setState({entries:entries});
-      console.log(this.state.entries);
+      entries.push([this.state.foodtype,1]);
+      this.props.changeFoodEntries(entries);
     }
   }
   removeFoodEntry(id){
-    let entries = this.state.entries;
+    let entries = this.props.foodEntries;
     entries.splice(id,1);
-    this.setState({entries:entries});
+    this.props.changeFoodEntries(entries);
   }
   selectType(event){
     console.log(event.target.value);
@@ -28,9 +29,9 @@ class FoodEntries extends Component{
   }
   render(){
     let elements = [];
-    for(let x in this.state.entries){
+    for(let x in this.props.foodEntries){
       elements.push(
-          <FoodEntry key = {x} keyvalue={x} type={this.state.entries[x]} remove = {this.removeFoodEntry}/>
+          <FoodEntry key = {x} keyvalue={x} type={this.state.foodtype} remove = {this.removeFoodEntry} entries={this.props.foodEntries} update = {this.props.changeFoodEntries}/>
         )
     }
   	return(
