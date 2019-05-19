2f01303d20feef5513e426d13348239300bf103c
David-Bayly <david.w.bayly@gmail.com>
2019-05-18 17:02:33 -0700

    completed add to database route.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 let app = express();
 const bodyParser = require("body-parser");
-let validator=require("valdator.js");
+let validator=require("./validator.js");
+let knex = require("./db/knex.js");
+let moment =require("moment") 
 //Setting up body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,8 +25,42 @@ app.get("/api/food",function(req,res){
 });
 
 app.post("/api/save",function(req,res){
-	let valid = 
+	//console.log(req.body);
+	let date =moment().format('YYYY-MM-DD');
+	console.log(date);
+	let valid = validator.validateEntry(req.body);
+	if(valid===true){
+
+		knex("feedingrecords").insert({
+			Date:date,
+			time:req.body.time,
+			Location:req.body.location,
+			"Number of Ducks":req.body.quantity,
+			Repeat:req.body.repeat
+		},'id').then(function(res){
+			console.log(res);
+			let foodentries = [];
+			for(let x in req.body.foodEntries){
+				foodentries.push({
+					"feeding record":res[0],
+					Type:req.body.foodEntries[0],
+					Quantity:req.body.foodEntries[1]
+				})	
+
+			}
+			knex("feedentries").insert(foodentries).then(function(result){
+				console.log(result);
+			})
+		})
+		res.send("Thank you for your submission!")
+	}else{
+		res.send(valid);
+	}
+
 });
+app.get("/api/data",function(req,res){
+
+})
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
