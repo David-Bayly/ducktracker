e82fff797a6dca095494cba4d727185302a32178
David-Bayly <david.w.bayly@gmail.com>
2019-05-18 17:49:03 -0700

    added bootstrap

--- migrations/20190518143018_feedingrecords.js
+++ migrations/20190518143018_feedingrecords.js
@@ -1,25 +1,10 @@
 
 exports.up = function(knex, Promise) {
 	return Promise.all([
-	    knex.schema.createTable('feedentries', (table) => {
-	      table.integer('feeding record').references('feedingrecords');
-	      table.string('Type');
-	      table.string('Quantity');
-	    }),
-	    knex.schema.createTable("feedingrecords",(table)=>{
-	    	table.increments();
-	    	table.dateTime('Date');
-	    	table.dateTime('Time');
-	    	table.integer('Number of Ducks');
-	    	table.string("Location");
-	    	table.string("Repeat");
-	    })
     ])
 };
 
 exports.down = function(knex, Promise) {
     return Promise.all([
-    knex.schema.dropTableIfExists('feedingrecords'),
-    knex.schema.dropTableIfExists('feedentries')
   ]);
 };
